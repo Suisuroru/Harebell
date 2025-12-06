c77d107fb19310244affcdb3f0382a726434d7b6
Bacteriawa <A3167717663@hotmail.com>
2025-12-07 04:13:25 +0800

    Luminol

--- src/main/kotlin/dev/menthamc/harebell/data/RepoTarget.kt
+++ src/main/kotlin/dev/menthamc/harebell/data/RepoTarget.kt
@@ -1,6 +1,6 @@
 package dev.menthamc.harebell.data
 
 data class RepoTarget(
-    val owner: String = "MenthaMC",
-    val repo: String = "Mint"
+    val owner: String = "LuminolMC",
+    val repo: String = "Luminol"
 )
